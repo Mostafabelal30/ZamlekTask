5dcdf80f5c95aa2a83715e3595ccd3a9f4d83c31
Mostafabelal30 <31407821+Mostafabelal30@users.noreply.github.com>
2018-02-21 00:59:02 +0200

    edit

---
+++ Zamlek/index.js
@@ -0,0 +1,67 @@
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+
+
+
+import { AppRegistry } from 'react-native';
+// import App from './App';
+
+
+import React, { Component } from 'react';
+import { StackNavigator } from 'react-navigation';
+  import Home from './screens/Home';
+import Splash from './screens/Splash';
+  import ItemDetials from './screens/ItemDetials';
+ import Login from './screens/Login';
+//  import Result from './screens/result';
+//  import Contact from './screens/contact';
+//  import VisaView from './screens/VisaView';
+//  import Tabs from './screens/Tabs';
+//  import Question from './screens/question';
+//  import FaqsCategory from './screens/FaqsCategory';
+//  import FaqsQuestion from './screens/FaqsQuestion';
+//  import FaqsAnswer from './screens/FaqsAnswer';
+//  import Thanks from './screens/thanks';
+//  import Jobs from './screens/Jobs';
+
+
+ 
+//  import Preparations from './screens/preparations';
+export const Stack=StackNavigator({
+ // Contact:{screen:Contact},
+ Login:{screen:Login},
+
+    Splash:{screen:Splash},
+    Home:{screen:Home},
+    ItemDetials:{screen:ItemDetials},
+   // Login:{screen:Login},
+    // Langauge:{screen:Langauge},
+    // Result:{screen:Result},
+    // VisaView:{screen:VisaView},
+    // Contact:{screen:Contact},
+    // Question:{screen:Question},
+    // FaqsCategory:{screen:FaqsCategory},
+    // FaqsQuestion:{screen:FaqsQuestion},
+    // FaqsAnswer:{screen:FaqsAnswer},
+    // Thanks:{screen:Thanks},
+    // Jobs:{screen:Jobs},
+
+
+    // Tabs:{screen:Tabs},
+    })
+    export default class Zamlek extends Component {
+        render() {
+          const { navigation } = this.props;
+          return (
+            
+            <Stack navigation={ navigation }/>
+          );
+        }
+      }
+
+
+
+
+
+
+AppRegistry.registerComponent('Zamlek', () => Zamlek);
